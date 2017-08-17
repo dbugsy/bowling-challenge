ef16caf0eec218c05f384650a781a96b584ad505
ddosque <dpbergquist@gmail.com>
2017-08-17 16:38:59 +0100

    add score for all ones

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,17 +1,29 @@
-import BowlingGame from '../src/BowlingGame';
+import BowlingGame from "../src/BowlingGame";
 
 describe(BowlingGame, () => {
-  const bowlingGame = new BowlingGame();
+  let bowlingGame;
 
-  it('plays a gutter game', () => {
+  beforeEach(() => {
+    bowlingGame = new BowlingGame();
+  });
+
+  it("plays a gutter game", () => {
     const gutterGameScore = 0;
     roll(0, 20);
-    expect(bowlingGame.score()).toEqual(gutterGameScore);
-  })
+
+    expect(bowlingGame.score).toEqual(gutterGameScore);
+  });
+
+  it("score is 20 after twenty ones", () => {
+    const expectedScore = 20;
+    roll(1, 20);
+
+    expect(bowlingGame.score).toEqual(expectedScore);
+  });
 
   const roll = (pins, numberOfRolls) => {
     for (let roll = 0; roll < numberOfRolls; roll++) {
       bowlingGame.roll(pins);
     }
-  }
+  };
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,10 +1,16 @@
 class BowlingGame {
-  roll() {
-    
+  constructor() {
+    this.rolls = [];
   }
 
-  score() {
-    return 0;
+  roll(pins) {
+   this.rolls.push(pins); 
+  }
+
+  get score() {
+    return this.rolls.reduce((score, roll) => {
+      return score + roll;
+    });
   }
 }
 
